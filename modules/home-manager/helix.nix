501190d60abd180878e7e873869def05cab43f49
Noah <pro@noahcode.dev>
2024-10-19 02:23:19 +0200

    theme helix
    disable helix and add image

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -1,13 +1,15 @@
 { pkgs, lib, config, ... }:
 
 {
+  stylix.targets.helix.enable = false;
+
   programs.helix = {
     enable = true;
     defaultEditor = true;
 
     settings = {
 
-    # Theme is handled by stylix
+      theme = "catppuccin-mocha";
 
       editor = {
         line-number = "relative";
@@ -56,6 +58,120 @@
       };
     };
 
+    themes = {
+      catppuccin-mocha = let
+        yellow = "#E5C07B";
+        blue = "#61AFEF";
+        red = "#E06C75";
+        purple = "#C678DD";
+        green = "#98C379";
+        gold = "#D19A66";
+        cyan = "#56B6C2";
+        white = "#ABB2BF";
+        bg = "#1E1E2F";
+        light-bg = "#32324e";
+        gray = "#3E4452";
+        faint-gray = "#3B4048";
+        light-gray = "#5C6370";
+        linenr = "#4B5263";
+      in {
+        "tag" = { fg = red; };
+        "attribute" = { fg = yellow; };
+        "comment" = { fg = light-gray; modifiers = ["italic"]; };
+        "constant" = { fg = cyan; };
+        "constant.numeric" = { fg = gold; };
+        "constant.builtin" = { fg = gold; };
+        "constant.character.escape" = { fg = gold; };
+        "constructor" = { fg = blue; };
+        "function" = { fg = blue; };
+        "function.builtin" = { fg = blue; };
+        "function.macro" = { fg = purple; };
+        "keyword" = { fg = purple; };
+        "keyword.control" = { fg = purple; };
+        "keyword.control.import" = { fg = red; };
+        "keyword.directive" = { fg = purple; };
+        "label" = { fg = purple; };
+        "namespace" = { fg = blue; };
+        "operator" = { fg = purple; };
+        "keyword.operator" = { fg = purple; };
+        "special" = { fg = blue; };
+        "string" = { fg = green; };
+        "type" = { fg = yellow; };
+        # "variable" = { fg = blue; };
+        "variable.builtin" = { fg = blue; };
+        "variable.parameter" = { fg = red; };
+        "variable.other.member" = { fg = cyan; };
+
+        "markup.heading" = { fg = red; };
+        "markup.raw.inline" = { fg = green; };
+        "markup.bold" = { fg = gold; modifiers = ["bold"]; };
+        "markup.italic" = { fg = purple; modifiers = ["italic"]; };
+        "markup.strikethrough" = { modifiers = ["crossed_out"]; };
+        "markup.list" = { fg = red; };
+        "markup.quote" = { fg = yellow; };
+        "markup.link.url" = { fg = cyan; modifiers = ["underlined"]; };
+        "markup.link.text" = { fg = purple; };
+
+        "diff.plus" = green;
+        "diff.delta" = gold;
+        "diff.minus" = red;
+
+        "diagnostic.info.underline" = { color = blue; style = "curl"; };
+        "diagnostic.hint.underline" = { color = green; style = "curl"; };
+        "diagnostic.warning.underline" = { color = yellow; style = "curl"; };
+        "diagnostic.error.underline" = { color = red; style = "curl"; };
+        "diagnostic.unnecessary" = { modifiers = ["dim"]; };
+        "diagnostic.deprecated" = { modifiers = ["crossed_out"]; };
+        "info" = { fg = blue; modifiers = ["bold"]; };
+        "hint" = { fg = green; modifiers = ["bold"]; };
+        "warning" = { fg = yellow; modifiers = ["bold"]; };
+        "error" = { fg = red; modifiers = ["bold"]; };
+
+        "ui.background" = { bg = bg; };
+        "ui.virtual" = { fg = faint-gray; };
+        "ui.virtual.indent-guide" = { fg = faint-gray; };
+        "ui.virtual.whitespace" = { fg = light-bg; };
+        "ui.virtual.ruler" = { bg = gray; };
+        "ui.virtual.inlay-hint" = { fg = light-gray; };
+        "ui.virtual.jump-label" = { fg = light-gray; modifiers = ["bold"]; };
+
+        "ui.cursor" = { fg = "#C3A2A2"; modifiers = ["reversed"]; };
+        "ui.cursor.primary" = { fg = white; modifiers = ["reversed"]; };
+        "ui.cursor.match" = { fg = blue; modifiers = ["underlined"]; };
+
+        "ui.selection" = { bg = "#4f1717"; };
+        "ui.selection.primary" = { bg = gray; };
+        "ui.cursorline.primary" = { bg = light-bg; };
+
+        "ui.highlight" = { bg = gray; };
+        "ui.highlight.frameline" = { bg = "#97202a"; };
+
+        "ui.linenr" = { fg = linenr; };
+        "ui.linenr.selected" = { fg = white; };
+
+        "ui.statusline" = { fg = white; bg = light-bg; };
+        "ui.statusline.inactive" = { fg = light-gray; bg = light-bg; };
+        "ui.statusline.normal" = { fg = light-bg; bg = blue; modifiers = ["bold"]; };
+        "ui.statusline.insert" = { fg = light-bg; bg = green; modifiers = ["bold"]; };
+        "ui.statusline.select" = { fg = light-bg; bg = purple; modifiers = [ "bold" ]; };
+        "ui.bufferline" = { fg = light-gray; bg = light-bg; };
+        "ui.bufferline.active" = { fg = light-bg; bg = blue; underline = { color = light-bg; style = "line"; }; };
+        "ui.bufferline.background" = { bg = light-bg; };
+
+        "ui.text" = { fg = white; };
+        "ui.text.focus" = { fg = white; bg = light-bg; modifiers = ["bold"]; };
+
+        "ui.help" = { fg = white; bg = gray; };
+        "ui.popup" = { bg = gray; };
+        "ui.window" = { fg = gray; };
+        "ui.menu" = { fg = white; bg = gray; };
+        "ui.menu.selected" = { fg = bg; bg = blue; };
+        "ui.menu.scroll" = { fg = white; bg = light-gray; };
+
+        "ui.debug" = { fg = red; };
+      };
+    };
+
     languages = {
 
       language = [
@@ -90,7 +206,6 @@
           formatter.command = "marksman";
         }
       ];
-
     };
   };
 
